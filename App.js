28cc13adcd843518ecb3cbda27d15e7d748378c9
Babyoilrig <93347177+Babyoilrig@users.noreply.github.com>
2022-02-19 12:33:36 +0000

    Trying to write a test to see if image has the correct source

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
 
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { Image, StyleSheet, Text, View, Button } from "react-native";
+import catandmartha from './assets/cat_and-martha.jpg';
+
+
+
 
 export default function App() {
+  //State to change text
+  const [outputText, setOutputText] = useState('Hello Martha!');
+
+  
   return (
     <View style={styles.container}>
-      <Text testID="myText">Hello World!</Text>
-     
+    <Image source={catandmartha} testID= "catAndMarthaImage" style={{ width: 305, height: 305 }} />
+      <Text style={{color:'#FEDD00', fontSize: 36}}>{outputText}</Text>
+      <Button
+        title="Change text"
+        onPress={() => setOutputText("You clicked me!")}
+      />
+      <Text>"Hello World!</Text>
     </View>
   );
 }
@@ -13,8 +27,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#FB48C4",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
+
--- App.test.js
+++ App.test.js
@@ -4,29 +4,65 @@ import renderer from 'react-test-renderer';
 
 import App from './App';
 
-
+//Test to check if the app is being rendered
 describe("App created", () => {
   it('has 1 child', () => {
     const tree = renderer.create(<App />).toJSON();
-    expect(tree.children.length).toBe(1);
+    expect(tree.children.length).toBe(4);
   });
 
+  //Test to check if what is being rendered in 'app' matches the snapshop in "_snapshot" file
   it('renders correctly', () => {
     const tree = renderer.create(<App />).toJSON();
     expect(tree).toMatchSnapshot();
   });
 
+  //Test to check whether the text 'Hello World!' is being rendered to the screen
   let findTextElement = function(tree, element){
     console.warn(tree)
     return true;
 }
  
 it('Find text element', ()=>{
-   let tree = renderer.create(
-       <App />
-   ).toJSON();
+   let tree = renderer.create(<App />).toJSON();
  
    expect(findTextElement(tree, 'Hello World!')).toBeDefined();
 })
+
+//Test to check source of image being rendered - not working
+
+let getByTestId = function(tree, testID){
+  console.warn(tree)
+  return true;
+}
   
+it('Find image element', ()=>{
+  let tree = renderer.create(
+      <App />
+  ).toJSON();
+
+  expect(getByTestId(tree, 'catAndMarthaImage')).toContain('src', './assets/cat_and-martha.jpg');
+})
 });
+
+//https://stackoverflow.com/questions/69732509/getbytestid-not-finding-testid
+
+//Possible format for uri image test
+
+// export const imageID = 'image_id';
+// ...
+
+// <Image testID={`${imageID}_${props.uri}`} ... />
+
+// Test
+// import {
+//   imageID
+// }, from '.';
+
+// ...
+
+
+// const testURI = 'https://picsum.photos/200';
+// const { getByTestId } = render(<Component uri={testURI} />);
+
+// expect(getByTestId()).toBeTruthy();
